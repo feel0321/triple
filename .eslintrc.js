199c100d186d7980c2396f03ff2d6c3530112c7a
고광필 <dishate@naver.com>
2022-07-04 20:56:14 +0900

    chore: eslint rule 수정 no-console warn, no-unused-vars warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 const createConfig = require('@titicaca/eslint-config-triple/create-config')
 
-module.exports = createConfig({ type: 'frontend' })
+const { extends: extendConfigs, overrides } = createConfig({ type: 'frontend' })
+
+module.exports = {
+  extends: [...extendConfigs],
+  overrides: [...overrides],
+  rules: {
+    'no-console': 1,
+    'no-unused-vars': 1,
+  },
+}
